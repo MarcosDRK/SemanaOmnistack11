3157893e4b28d708fb3fbfb51cf8cf0ff82045b2
MarcosDRK <marcos.ege@live.com>
2020-03-25 12:41:58 -0300

    teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import logo from './starter.svg';
-import './App.css';
+
+import './global.css';
+
+import Logon from './pages/Logon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-font">
-          Ops esta página está em construção!
-          <p className="App-font">
-            Tente entrar neste link para mais informações:
-          </p>
-        </p>
-
-        <a
-          className="App-link"
-          href="https://rocketseat.com.br/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          RocketSeat Online
-        </a>
-      </header>
-    </div>
+    <Logon />
   );
 }
 
